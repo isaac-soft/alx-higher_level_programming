f63e3270142b18926a52826e3341dd2c431a3667
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-09-23 15:06:35 +0000

    Fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" " if i < 99 else "")
+            print("FizzBuzz", end=" " if i < 99 else " ")
         elif i % 3 == 0:
-            print("Fizz", end=" " if i < 99 else "")
+            print("Fizz", end=" " if i < 99 else " ")
         elif i % 5 == 0:
-            print("Buzz", end=" " if i < 99 else "")
+            print("Buzz", end=" " if i < 99 else " ")
         else:
-            print(i, end=" " if i < 99 else "")
+            print(i, end=" " if i < 99 else " ")
