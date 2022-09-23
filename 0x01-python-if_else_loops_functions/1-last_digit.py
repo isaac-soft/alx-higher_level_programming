f11a1966851e042455babfdf1bf2379f4ff6d22a
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-09-23 07:13:03 +0000

    check value of last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# extract the last digit
+last_digit = int(str(number)[-1])
+# setting the text property for each condition
+if last_digit > 5:
+    text = "and is greater than 5"
+elif last_digit == 0:
+    text = "and is 0"
+elif (last_digit < 6) and (last_digit != 0):
+    text = "and is less than 6 and not 0"
+# print the output
+print("Last digit of {} is {} {}".format(number, last_digit, text))
