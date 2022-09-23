32a52018a211890c9d416938651a1b39409e1634
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-09-23 09:24:05 +0000

    print lower case except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for char in range(ord('a'), ord('z') + 1):
     if chr(char) == 'q' or chr(char) == 'e':
         continue
-    print("{:s}".format(chr(char)), end="")
+    print("{:c}".format(char), end="")
