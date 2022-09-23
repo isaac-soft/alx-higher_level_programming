61b28c1ae56986e4afd77c25f4ed108b322ae15c
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-09-23 11:59:42 +0000

    print all possible combinations of two digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
+        #skip combination repetitions
         if i >= j:
             continue
+        # break 1st loop if at the last combination
         if i == max(range(10)) - 1:
             break
+        # print combinations
         print("{}{}".format(i, j), end=", ")
+    # break second loop if at the last combination
     if i == max(range(10)) - 1:
         break
+#print the last combination
 print("{}{}".format(i, j))
+
+# Alternatively
+# for num in range(9):
+#   for var in range(num + 1, 10):
+#       print("{:d}{:d}".format(num, var), end = '\n' if num == 8 else ', ')
